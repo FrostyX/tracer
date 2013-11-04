41d1c1709022a7d3198d01d83f88701b05a840cd
Jakub Kadlčík <frostyx@email.cz>
2013-11-04 18:17:11 +0100

    Load YUM module

---
+++ bin/tracer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Tracer finds outdated running packages in your system
+# Copyright 2013 Jakub Kadlčík
+
+# Enable importing modules from parent directory (tracer's root directory)
+import sys
+sys.path.append('.')
+
+# Tracer modules
+import packageManagers.yum as yum
+
+
+print "Hello world"
---
+++ packageManagers/yum.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Module to working with yum package manager
+# Copyright 2013 Jakub Kadlčík
