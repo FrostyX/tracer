dc3b3c5709ab29ce8a2007349262985242ba7b37
Jakub Kadlčík <frostyx@email.cz>
2013-11-06 14:16:24 +0100

    Implement function _database_file()

--- packageManagers/yum.py
+++ packageManagers/yum.py
@@ -2,6 +2,7 @@
 """Module to work with yum package manager class
 Copyright 2013 Jakub Kadlčík"""
 
+from os import listdir
 from ipackageManager import IPackageManager
 
 class Yum(IPackageManager):
@@ -13,9 +14,14 @@ class Yum(IPackageManager):
 		return 0
 
 	def _transactions_newer_than(self, unix_time):
-		"""Returns list of transactions which were modified between unix_time and present"""
+		"""Returns list of transactions which ran between unix_time and present"""
 		return 0
 
 	def _package_files(self, pkg_name):
 		"""Returns list of files provided by package"""
 		return 0
+
+	def _database_file(self):
+		"""Returns path to yum history database file"""
+		history_path = '/var/lib/yum/history/'
+		return history_path + os.listdir(history_path)[-1]
