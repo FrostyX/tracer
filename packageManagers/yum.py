a2aae2453c6ec38faec86df285cdda7c192d7b8b
Jakub Kadlčík <frostyx@email.cz>
2013-11-06 14:56:46 +0100

    Implement function _transactions_newer_than()

--- packageManagers/yum.py
+++ packageManagers/yum.py
@@ -4,6 +4,7 @@ Copyright 2013 Jakub Kadlčík"""
 
 from os import listdir
 from ipackageManager import IPackageManager
+import sqlite3
 
 class Yum(IPackageManager):
 
@@ -14,8 +15,24 @@ class Yum(IPackageManager):
 		return 0
 
 	def _transactions_newer_than(self, unix_time):
-		"""Returns list of transactions which ran between unix_time and present"""
-		return 0
+		"""
+		Returns list of transactions which ran between unix_time and present.
+		Requires root permissions.
+		"""
+
+		sql = """
+			SELECT trans_beg.tid, trans_beg.timestamp AS beg, trans_end.timestamp AS end
+			FROM trans_beg JOIN trans_end ON trans_beg.tid=trans_end.tid
+			WHERE beg > ?
+			ORDER BY trans_beg.tid
+		"""
+
+		sqlite = self._database_file()
+		conn = sqlite3.connect(sqlite)
+		conn.row_factory = sqlite3.Row
+		c = conn.cursor()
+		c.execute(sql, [unix_time])
+		return c.fetchall()
 
 	def _package_files(self, pkg_name):
 		"""Returns list of files provided by package"""
@@ -24,4 +41,4 @@ class Yum(IPackageManager):
 	def _database_file(self):
 		"""Returns path to yum history database file"""
 		history_path = '/var/lib/yum/history/'
-		return history_path + os.listdir(history_path)[-1]
+		return history_path + listdir(history_path)[-1]
