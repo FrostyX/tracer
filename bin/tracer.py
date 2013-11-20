19005c8f5ca2c63712bf1586ca51578fa656bd9a
Jakub Kadlčík <frostyx@email.cz>
2013-11-20 18:47:15 +0100

    Print modified packages

--- bin/tracer.py
+++ bin/tracer.py
@@ -8,8 +8,28 @@ import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0, parentdir)
 
+# System modules
+import re
+import psutil
+
 # Tracer modules
 from packageManagers.yum import Yum
+import resources.memory as memory
 
+def modified(manager):
+	packages = manager.packages_newer_than(psutil.BOOT_TIME)
+	files_in_memory = memory.files_in_memory()
 
-print "Hello world"
+	modified = []
+	for package in packages:
+		for file in manager._package_files(package['name']):
+
+			regex = re.compile('^' + re.escape(file) + "(\.*|$)")
+			if memory.is_in_memory(regex, files_in_memory):
+				modified.append(package['name'])
+				break;
+	return modified
+
+# More times a package is updated the more times it is contained in a package list.
+for package in set(modified(Yum())):
+	print package
