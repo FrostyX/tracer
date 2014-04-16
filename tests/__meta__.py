52613cfd1a470278add9a45fea70fcebb4fb4fa9
Jakub Kadlčík <frostyx@email.cz>
2014-04-16 12:27:18 +0200

    Add meta file for tests; You should import it in your tests

---
+++ tests/__meta__.py
@@ -0,0 +1,6 @@
+# Enable importing modules from parent directory (tracer's root directory)
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+os.sys.path.insert(0, parentdir)
+
+import unittest
