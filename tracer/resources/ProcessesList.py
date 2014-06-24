8d1811f9fb7f4a2aa9435743b99d471fcd22ba5d
Jakub Kadlčík <frostyx@email.cz>
2014-06-24 22:24:54 +0200

    Implement ProcessList collection

---
+++ tracer/resources/ProcessesList.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+
+from tracer.resources.applications import Applications
+
+class ProcessesList(list):
+
+	def __init__(self, *args):
+		list.__init__(self, *args)
+
+	def exclude_type(self, app_type):
+		"""app_type -- see Applications.TYPES"""
+		without = []
+		for p in self:
+			a = Applications.find(p.name)
+			if a["type"] != app_type:
+				without.append(p)
+		return without
+
+	def exclude_types(self, app_types):
+		"""app_types -- see Applications.TYPES"""
+		without = []
+		for p in self:
+			a = Applications.find(p.name)
+			if a["type"] not in app_types:
+				without.append(p)
+		return without
+
+	def count_type(self, app_type):
+		count = 0
+		for p in self:
+			a = Applications.find(p.name)
+			if a['type'] == app_type:
+				count += 1
+		return count
