78bf96868e097aaab445dde96ab4a9d9e4c2e1a5
Jakub Kadlčík <frostyx@email.cz>
2014-07-21 12:18:45 +0200

    Remove duplicated output

--- integration/dnf/plugins/tracer.py
+++ integration/dnf/plugins/tracer.py
@@ -70,6 +70,7 @@ def _print_output(out):
 		print "  " + _("nothing_to_restart")
 		return
 
+	# First line is _("you_should_restart")
 	# Last value is blank line
-	for line in out.split('\n')[:-1]:
-		print "  " + line
+	for line in out.split('\n')[1:-1]:
+		print line
