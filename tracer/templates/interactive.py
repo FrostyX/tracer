f2153098b1c41ad77e147281f2f3fe479ab40fe4
Jakub Kadlčík <frostyx@email.cz>
2014-08-10 20:51:38 +0200

    Add template for interactive mode

---
+++ tracer/templates/interactive.py
@@ -0,0 +1,19 @@
+from tracer.resources.lang import _
+import tracer.templates.note_for_hidden
+
+def render(processes=None, args=None, total_count=None, session_count=None, static_count=None):
+	i = 1
+	digits = len(str(len(processes)))
+	for process in processes:
+		n = "[{0}]".format(i).ljust(digits + 2)
+		print "{} {}".format(n, process.name)
+		i += 1
+
+	if not args.all: tracer.templates.note_for_hidden.render(
+		args = args,
+		total_count = total_count,
+		session_count = session_count,
+		static_count = static_count
+	)
+
+	print "\n" + _("prompt_help")
