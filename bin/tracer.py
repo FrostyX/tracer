f6a53b0bcc23c92d394a2a23a30db1c8944e8018
Jakub Kadlčík <frostyx@email.cz>
2014-08-12 15:49:31 +0200

    Use templates to print output

--- bin/tracer.py
+++ bin/tracer.py
@@ -36,6 +36,11 @@ from tracer.resources.ProcessesList import ProcessesList
 import tracer.resources.memory as Memory
 import tracer.resources.system as System
 
+import tracer.templates.default
+import tracer.templates.helper
+import tracer.templates.interactive
+import tracer.templates.note_for_hidden
+
 
 def main(argv=sys.argv, stdin=[]):
 	# If there is something on stdin (that means piped into tracer)
@@ -56,104 +61,81 @@ def main(argv=sys.argv, stdin=[]):
 		processes = ProcessesList(tracer.trace_running(_user(args.user)))
 		if not processes: return
 
-		if not args.helpers: print _("you_should_restart")
-		if args.interactive: _print_all_interactive(processes, args)
+		if args.helpers: _print_helpers(processes, args)
+		elif args.interactive: _print_all_interactive(processes, args)
 		else: _print_all(processes, args)
 
 	except (UnsupportedDistribution, PathNotFound, LockedDatabase) as ex:
 		print ex
+
 
 def _user(user):
 	if   user == '*':    return None
 	elif user == None:   return os.getlogin()
 	else: return user[0]
 
-def _print_all(processes, args):
-	filtered = processes.exclude_types([
+
+def _processes(processes, args):
+	return processes.exclude_types([
 		Applications.TYPES['STATIC'],
 		Applications.TYPES['SESSION']
 	]) if not args.all else processes
 
-	for process in filtered:
-		if args.helpers:
-			print_helper(process.name)
-		else:
-			print "  " + process.name
 
-	if not args.all: _print_note_for_hidden(
-		len(processes),
-		processes.count_type(Applications.TYPES['SESSION']),
-		processes.count_type(Applications.TYPES['STATIC'])
+def _print_all(processes, args):
+	filtered = _processes(processes, args)
+
+	tracer.templates.default.render(
+		processes = filtered,
+		args = args,
+		total_count = len(processes),
+		session_count = processes.count_type(Applications.TYPES['SESSION']),
+		static_count = processes.count_type(Applications.TYPES['STATIC'])
 	)
 
+
+def _print_helpers(processes, args):
+	for process in _processes(processes, args):
+		print_helper(process.name)
+		print ""
+
+	tracer.templates.note_for_hidden.render(
+		args = args,
+		total_count = len(processes),
+		session_count = processes.count_type(Applications.TYPES['SESSION']),
+		static_count = processes.count_type(Applications.TYPES['STATIC'])
+	)
+
+
 def _print_all_interactive(processes, args):
-	filtered = processes.exclude_types([
-		Applications.TYPES['STATIC'],
-		Applications.TYPES['SESSION']
-	]) if not args.all else processes
+	filtered = _processes(processes, args)
 
 	while True:
-		i = 1
-		digits = len(str(len(filtered)))
-		for process in filtered:
-			n = "[{0}]".format(i).ljust(digits + 2)
-			print "{} {}".format(n, process.name)
-			i += 1
-
-		if not args.all: _print_note_for_hidden(
-			len(processes),
-			processes.count_type(Applications.TYPES['SESSION']),
-			processes.count_type(Applications.TYPES['STATIC'])
+		tracer.templates.interactive.render(
+			processes = filtered,
+			args = args,
+			total_count = len(processes),
+			session_count = processes.count_type(Applications.TYPES['SESSION']),
+			static_count = processes.count_type(Applications.TYPES['STATIC'])
 		)
 
-		print "\n" + _("prompt_help")
 		answer = raw_input("--> ")
 		try:
 			if answer == "q": return
-			elif int(answer) <= 0 or int(answer) > i: raise IndexError
+			elif int(answer) <= 0 or int(answer) > len(filtered): raise IndexError
 			print_helper(filtered[int(answer) - 1].name)
 
 		except (SyntaxError, IndexError, ValueError):
 			print _("wrong_app_number")
 
-		raw_input(_("press_enter"))
+		raw_input("\n" + _("press_enter"))
 
-def _print_note_for_hidden(total_count, session_count, static_count):
-	if not args.quiet and (session_count > 0 or static_count > 0):
-		if session_count + static_count != total_count:
-			print ""
-
-		print _("note_unlisted_apps")
-		if session_count > 0:
-			print _("requiring_session").format(session_count)
-
-		if static_count > 0:
-			print _("requiring_reboot").format(static_count)
-
-def _affected_by_str(app_name):
-	if args.verbose == 0:
-		return ""
-
-	tracer = Tracer()
-	affected_by = tracer.who_affected(app_name)
-
-	indent = "    "
-	s = "\n" + indent + _("affected_by") + ":\n"
-	for package in affected_by:
-		s += 2 * indent + package + "\n"
-
-		if args.verbose < 2:
-			continue
-
-		for file in affected_by[package]:
-			s += 3 * indent + file + "\n"
-	return s
 
 def print_helper(app_name):
 	process = Memory.process_by_name(app_name)
 	if process:
-		tracer = Tracer()
-		package = tracer.package_info(app_name)
+		tr = Tracer()
+		package = tr.package_info(app_name)
 		app = Applications.find(app_name)
 
 		now = datetime.datetime.fromtimestamp(time.time())
@@ -170,21 +152,17 @@ def print_helper(app_name):
 		elif started.seconds >= 0:
 			started_str = str(started.seconds) + " seconds"
 
-		affected_by = _affected_by_str(app_name)
 		how_to_restart = app['helper'] if app['helper'] else _("not_known_restart")
 
-		print _("helper").format(
-			app_name = app_name,
-			pkg_name = package.name,
-			type = app["type"].capitalize(),
-			pkg_description = package.description,
-			user = process.username,
+		tracer.templates.helper.render(
+			args = args,
+			process = process,
+			application = app,
+			package = package,
 			time = started_str,
-			pid = process.pid,
-			affected_by = affected_by,
-			how_to_restart = how_to_restart,
+			affected_by = tr.who_affected(app_name),
+			how_to_restart = how_to_restart
 		)
-
 	else:
 		print _("app_not_running").format(app_name)
 
