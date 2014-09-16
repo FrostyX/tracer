d5231a3633adc5f3366e7f5b1cc22de23910d9a5
Jakub Kadlčík <frostyx@email.cz>
2014-09-17 00:51:29 +0200

    Print 'you should restart' again

--- tracer/views/default.py
+++ tracer/views/default.py
@@ -10,18 +10,21 @@ class DefaultView(View):
 		without_helpers = self.args.processes.without_helpers()
 		note = self.args.session_count or self.args.static_count
 
+		if with_helpers or without_helpers:
+			print _("you_should_restart") + "\n"
+
 		if len(with_helpers):
-			print _("restart_using_helpers")
+			print "  " + _("restart_using_helpers")
 			for process in with_helpers:
-				print "  " + with_helpers.application(process).helper
+				print "      " + with_helpers.application(process).helper
 
 			if without_helpers or note:
 				print ""
 
 		if len(without_helpers):
-			print _("restart_manually")
+			print "  " + _("restart_manually")
 			for process in without_helpers:
-				print "  " + process.name
+				print "      " + process.name
 
 		if not self.args.args.all:
 			view = NoteForHiddenView()
