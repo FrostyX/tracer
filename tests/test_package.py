db45a39dbb2383ef655bd2216f6d3548416a231c
Jakub Kadlčík <frostyx@email.cz>
2015-02-19 15:17:22 +0100

    Add some package tests

---
+++ tests/test_package.py
@@ -0,0 +1,17 @@
+from __meta__ import *
+from tracer.resources.package import Package
+
+
+class TestPackage(unittest.TestCase):
+
+	def test_equality(self):
+		p1 = Package("foo")
+		p2 = Package("foo")
+		p3 = Package("bar")
+
+		self.assertEquals(p1, p2)
+		self.assertNotEquals(p1, p3)
+
+	def test_representation(self):
+		package = Package("foo")
+		self.assertEquals(repr(package), "<Package:foo>")
