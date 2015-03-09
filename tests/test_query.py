2a8d5b0263a3c76d90d2b73ac3b16a44f1bc45a5
Jakub Kadlčík <frostyx@email.cz>
2015-03-09 19:28:05 +0100

    Add tests for Query

---
+++ tests/test_query.py
@@ -0,0 +1,28 @@
+from __meta__ import *
+from tracer.query import Query, Lazy
+
+
+class TestQuery(unittest.TestCase):
+
+	def setUp(self):
+		self.query = Query()
+		self.query._tracer = TracerMock()
+
+	def test_affected_applications(self):
+		apps_query = self.query.affected_applications()
+		self.assertIsInstance(apps_query, Lazy)
+		self.assertTrue(hasattr(apps_query, "get"))
+		self.assertListEqual(list(apps_query.get()), ["A", "B", "C"])
+
+
+class TracerMock(object):
+	def __init__(self, *args):
+		pass
+
+	def trace_affected(self, user=None):
+		return ["A", "B", "C"]
+
+
+
+if __name__ == '__main__':
+	unittest.main()
