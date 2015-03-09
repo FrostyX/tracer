636c34a8e3d18fca2da3ddc285d4d4f435576f04
Jakub Kadlčík <frostyx@email.cz>
2015-03-09 17:48:52 +0100

    Add a Query class

---
+++ tracer/query.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+# query.py
+# Module providing quering operations to Tracer API
+#
+# Copyright (C) 2015 Jakub Kadlčík
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+from tracer.resources.tracer import Tracer
+from tracer.resources.system import System
+from tracer.resources.rules import Rules
+from tracer.resources.applications import Applications
+from tracer.resources.memory import dump_memory
+
+
+class Query(object):
+	"""
+	Provide API for Tracer quering operations.
+	They are executed kind of lazily, so running the operation will
+	return just an wrapper class with ``get()`` method.
+
+	Example::
+
+		from tracer.query import Query
+		q = Query()
+		q.affected_applications().get()
+
+	.. note::
+		Some quering methods can require root permissions
+
+	"""
+
+	_tracer = Tracer(System.package_manager(), Rules, Applications, dump_memory)
+
+	def affected_applications(self, user=None):
+		"""
+		Return list of applications which use some outdated files
+		"""
+		return Lazy(self._tracer.trace_affected, {"user": user})
+
+
+class Lazy(object):
+	def __init__(self, method, kwargs):
+		self._method = method
+		self._kwargs = kwargs
+
+	def get(self):
+		return self._method(**self._kwargs)
