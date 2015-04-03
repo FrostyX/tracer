440d9b03fb6966f1af081f00e4802fbe4ea3c112
Jakub Kadlčík <frostyx@email.cz>
2015-04-03 02:49:08 +0200

    Python3: Fix forgotten prints

--- tracer/views/resource/packages.py
+++ tracer/views/resource/packages.py
@@ -5,10 +5,10 @@ from datetime import datetime
 class PackagesView(View):
 	def render(self):
 		line = "{0: <15}{1: <10}{2: <40}"
-		print line.format("Date", "Time", "Package name")
-		print 55 * "-"
+		print(line.format("Date", "Time", "Package name"))
+		print(55 * "-")
 		for package in self.args.packages:
 			modified = datetime.fromtimestamp(package.modified)
 			date = modified.strftime('%Y-%m-%d')
 			time = modified.strftime('%H:%M')
-			print line.format(date, time, package.name)
+			print(line.format(date, time, package.name))
