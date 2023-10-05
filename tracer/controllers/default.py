8c759f6fcd7843a80125d17b943b9bf18e37e75d
Jakub Kadlcik <frostyx@email.cz>
2023-10-05 02:11:29 +0200

    Fix parameters for specifying the target user
        tracer -e
        tracer -r
        tracer -u nosuchuser
    
    The `or '*'` condition was always true.

--- tracer/controllers/default.py
+++ tracer/controllers/default.py
@@ -153,7 +153,7 @@ class DefaultController(object):
 		]) if not args.all else applications
 
 	def _user(self, user):
-		if   user == ['*'] or '*':    return None
+		if   user == ['*'] or user == '*':    return None
 		elif user == 'root': return user
 		elif not user:       return System.user()
 		else: return user[0]
